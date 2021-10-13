140a686095106f5825cd23a92eb3bffdfea8b36c
ahmed alhalfa <ah.alhalfa@gmail.com>
2021-10-13 18:39:25 +0200

    fixed a bug in the isAuth middleware

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -4,11 +4,14 @@ const { errorHandler } = require("../utils");
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    return errorHandler(
-      next,
-      "you are not authenticated, please log in proberly",
-      401
-    );
+    // return errorHandler(
+    //   next,
+    //   "you are not authenticated, please log in proberly",
+    //   401
+    // );
+    const error = new Error("you are not authenticated, please log in proberly");
+    error.statusCode = 401;
+    throw error;
   }
   const token = authHeader.split(" ")[1];
   let decodedToken;
