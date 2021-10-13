5e67ba8ba761d5283467d08444c1d279e77d9cbd
ahmed alhalfa <ah.alhalfa@gmail.com>
2021-10-13 19:09:42 +0200

    hotfix to the isAuth bug

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -4,21 +4,29 @@ const { errorHandler } = require("../utils");
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    return res.status(401).json({ message: "please log in" });
+    return errorHandler(
+      next,
+      "you are not authenticated, please log in proberly",
+      401
+    );
   }
   const token = authHeader.split(" ")[1];
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, process.env.JWT_SECRET);
   } catch (err) {
-    return res.status(401).json({ message: err });
+    return errorHandler(
+      next,
+      "you are not authenticated, please log in proberly",
+      401
+    );
   }
   if (!decodedToken) {
-    // return errorHandler(
-    //   next,
-    //   "you are not authenticated, please log in proberly",
-    //   401
-    // );
+    return errorHandler(
+      next,
+      "you are not authenticated, please log in proberly",
+      401
+    );
   }
   req.userId = decodedToken.userId;
   next();
